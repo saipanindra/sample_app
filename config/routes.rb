bd594dbabdc117eaad5fd810762aa958fcece6e5
saipanindra <saipanindra9@gmail.com>
2013-07-29 19:23:57 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  #get "users/new"
+  root :to => 'static_pages#home'
+  match '/help' ,  to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via:'get'
+  match '/contact', to: 'static_pages#contact', via:'get'
+  match '/signup', to: 'users#new', via: 'get'
+  #get "static_pages/home"
 
-  get "static_pages/help"
+  #get "static_pages/help"
 
-  get "static_pages/about"
+  #get "static_pages/about"
   
-  get "static_pages/contact"
+  #get "static_pages/contact"
   # The priority is based upon order of creation:
   
   # first created -> highest priority.
@@ -14,8 +20,7 @@ SampleApp::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # Sample of named route:  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
