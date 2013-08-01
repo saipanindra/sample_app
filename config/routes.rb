395288633810b60170efbe6263cf93c4d7bc290a
saipanindra <saipanindra9@gmail.com>
2013-08-01 16:39:24 -0400

    Remove modeling-users branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "user/name:string"
+  get "user/email:string"
   #get "users/new"
   root :to => 'static_pages#home'
   match '/help' ,  to: 'static_pages#help', via: 'get'
